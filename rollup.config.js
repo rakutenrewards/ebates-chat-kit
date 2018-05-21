841e012923fe32329849dc0ba5e3882470571d76
Eran Kampf <eran@ekampf.com>
2018-05-21 15:18:08 -0700

    Fixed build: styled-component should be external

--- rollup.config.js
+++ rollup.config.js
@@ -7,33 +7,40 @@ import globals from 'rollup-plugin-node-globals'
 
 const isProduction = process.env.NODE_ENV === 'production';
 
+const commonOutput = {
+  sourcemap: true,
+  globals: {
+    'react': 'React',
+    'react-dom': 'ReactDOM',
+    'styled-components': 'styled'
+  }
+}
+
 
 export default {
   input: 'src/index.js',
   watch: {
     include: 'src/**/*.*'
   },
-  sourcemap: true,
   output: [
     {
       file: 'dist/ebatesChatKit.js',
-      format: 'cjs'
+      format: 'cjs',
+      ...commonOutput
     },
     {
       file: 'dist/ebatesChatKit.umd.js',
       format: 'umd',
-      name: 'EbatesChatKit'
+      name: 'EbatesChatKit',
+      ...commonOutput
     },
     {
       file: 'dist/ebatesChatKit.es.js',
-      format: 'es'
+      format: 'es',
+      ...commonOutput
     }
   ],
-  external: ['react', 'react-dom'],
-  globals: {
-    react: 'React',
-    'react-dom': 'ReactDOM'
-  },
+  external: ['react', 'react-dom', 'styled-components'],
   plugins: [
     babel({
       babelrc: false,
