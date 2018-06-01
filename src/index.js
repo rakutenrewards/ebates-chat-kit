9dc120b3db456a28ee0e5fd0e02822397e96632d
Eran Kampf <eran@ekampf.com>
2018-06-01 15:35:56 -0700

    Expose all controls

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import Chat from './components/Chat.js';
+import MessageList from './components/MessageList';
+import MessageGroup from './components/MessageGroup';
+import QuickReplies from './components/QuickReplies';
+import * as MessageControls from './components/Message';
 
-const components = Object.assign({}, { Chat });
+const components = Object.assign({}, MessageControls, { Chat, MessageList, MessageGroup, QuickReplies });
 
 export default components;
